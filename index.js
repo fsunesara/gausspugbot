56db1ce26b093dad8684ba1666d49aea1747308b
Faisal <fss170000@utdallas.edu>
2020-07-19 16:50:38 -0500

    Fix security issue and add config.json usability.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const {prefix, token} = require('./config.json');
 
 client.once('ready', () => {
 	console.log('Ready');
+
 });
 
-//client.login(); REMOVED TOKEN OOPS
+client.login(token);
 
 client.on('message', message => {
 	console.log(message.content);
